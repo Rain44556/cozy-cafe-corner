ba62924f74d807bcd9f3ed704db6e02d0f52a158
rain44556 <bristy44556@gmail.com>
2024-12-02 12:31:45 +0600

    created API for users

--- index.js
+++ index.js
@@ -22,11 +22,21 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
     const coffeeCollection = client.db('coffeeDB').collection('coffee');
+    const userCollection = client.db('coffeeDB').collection('users');
+
 
     app.get('/coffee', async (req, res) => {
       const cursor = coffeeCollection.find();
@@ -76,9 +86,15 @@ async function run() {
     })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    //user API
+    app.post('/users', async(req,res)=>{
+      const newUser = req.body;
+     const result = await userCollection.insertOne(newUser);
+     res.send(result);
+    })
+
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
